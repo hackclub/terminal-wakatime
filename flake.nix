dbc9e7ce7f6ace6c883028f8b7c725542ee10c77
Kieran Klukas <me@dunkirk.sh>
2025-06-27 09:44:04 -0400

    chore: bump flake version to 1.1.6

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       packages = forAllSystems ({ pkgs }: {
         default = pkgs.buildGoModule {
           pname = "terminal-wakatime";
-          version = "1.1.5";
+          version = "1.1.6";
           subPackages = [ "cmd/terminal-wakatime" ];
           src = self;
           vendorHash = "sha256-fchZVBY43ccu6nbWn572Qzfgeq4uIwpLf99lOuJCO44=";
